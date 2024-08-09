dde9755c3ef749dd5f12ae460fd0aaa98cd4bff8
Matt Woolley <matt.wool@live.com.au>
2024-08-10 01:57:07 +1000

    Allow configuration of data directory

--- graft/local_file/local_file.py
+++ graft/local_file/local_file.py
@@ -2,7 +2,9 @@
 
 import enum
 import json
+import os
 import pathlib
+import platform
 import shutil
 import tempfile
 from collections.abc import Callable, Generator, Iterable
@@ -11,8 +13,11 @@ from typing import Any, Final, TypedDict, override
 from graft import architecture, domain, graphs
 from graft.domain import tasks
 
+_DATA_DIRECTORY_PATH_ENVIRONMENT_VARIABLE_KEY: Final = "GRAFT_DATA_DIRECTORY_PATH"
+
 _FIRST_TASK: Final = tasks.UID(1)
 
+_APP_DIRECTORY_NAME: Final = "graft"
 _DATA_DIRECTORY_NAME: Final = "data"
 
 _TASK_HIERARCHY_GRAPH_FILENAME: Final = "task_hierarchy_graph.json"
@@ -20,18 +25,29 @@ _TASK_DEPENDENCY_GRAPH_FILENAME: Final = "task_dependency_graph.json"
 _TASK_ATTRIBUTES_REGISTER_FILENAME: Final = "task_attributes_register.json"
 _UNUSED_TASK_FILENAME: Final = "task_next_uid.txt"
 
-_DATA_DIRECTORY_PATH = pathlib.Path.cwd() / _DATA_DIRECTORY_NAME
 
-_TASK_HIERARCHY_GRAPH_FILEPATH: Final = (
-    _DATA_DIRECTORY_PATH / _TASK_HIERARCHY_GRAPH_FILENAME
-)
-_TASK_DEPENDENCY_GRAPH_FILEPATH: Final = (
-    _DATA_DIRECTORY_PATH / _TASK_DEPENDENCY_GRAPH_FILENAME
-)
-_TASK_ATTRIBUTES_REGISTER_FILEPATH: Final = (
-    _DATA_DIRECTORY_PATH / _TASK_ATTRIBUTES_REGISTER_FILENAME
-)
-_UNUSED_TASK_FILEPATH: Final = _DATA_DIRECTORY_PATH / _UNUSED_TASK_FILENAME
+def _get_data_directory() -> pathlib.Path:
+    """Get the directory where system data is stored.
+
+    Will return the directory specified by the environment variable if
+    available, otherwise will fall back on defaults for standard OS's.
+    """
+    if _DATA_DIRECTORY_PATH_ENVIRONMENT_VARIABLE_KEY in os.environ:
+        return pathlib.Path(os.environ[_DATA_DIRECTORY_PATH_ENVIRONMENT_VARIABLE_KEY])
+
+    match platform.system():
+        case "Windows":
+            return (
+                pathlib.Path(os.environ["LOCALAPPDATA"])
+                / _APP_DIRECTORY_NAME
+                / _DATA_DIRECTORY_NAME
+            )
+        case _:
+            # TODO: Add Linux and Max support
+            pass
+
+    # TODO: Raise proper exception
+    raise ValueError("OS is not windows, and not currently supported")
 
 
 class PartiallyInitialisedError(Exception):
@@ -212,30 +228,47 @@ class LocalFileDataLayer(architecture.DataLayer):
 
         If the filesystem has not been initialised, will do so automatically.
         """
+        self._data_directory = _get_data_directory()
 
-        def get_initialisation_status() -> InitialisationStatus:
-            """Get the initialisation status of the local filesystem."""
-            if not _DATA_DIRECTORY_PATH.exists():
-                return InitialisationStatus.NOT_INITIALISED
-
-            return (
-                InitialisationStatus.FULLY_INITIALISED
-                if (
-                    _TASK_HIERARCHY_GRAPH_FILEPATH.exists()
-                    and _TASK_DEPENDENCY_GRAPH_FILEPATH.exists()
-                    and _TASK_ATTRIBUTES_REGISTER_FILEPATH.exists()
-                    and _UNUSED_TASK_FILEPATH.exists()
-                )
-                else InitialisationStatus.PARTIALLY_INITIALISED
-            )
-
-        match get_initialisation_status():
+        match self._get_initialisation_status():
             case InitialisationStatus.NOT_INITIALISED:
-                _initialise()
+                self._initialise()
             case InitialisationStatus.PARTIALLY_INITIALISED:
                 raise PartiallyInitialisedError
             case InitialisationStatus.FULLY_INITIALISED:
                 pass
+
+    @property
+    def _task_attributes_register_file(self) -> pathlib.Path:
+        return self._data_directory / _TASK_ATTRIBUTES_REGISTER_FILENAME
+
+    @property
+    def _task_hierarchy_graph_file(self) -> pathlib.Path:
+        return self._data_directory / _TASK_HIERARCHY_GRAPH_FILENAME
+
+    @property
+    def _task_dependency_graph_file(self) -> pathlib.Path:
+        return self._data_directory / _TASK_DEPENDENCY_GRAPH_FILENAME
+
+    @property
+    def _unused_task_file(self) -> pathlib.Path:
+        return self._data_directory / _UNUSED_TASK_FILENAME
+
+    def _get_initialisation_status(self) -> InitialisationStatus:
+        """Get the initialisation status of the local filesystem."""
+        if not self._data_directory.exists():
+            return InitialisationStatus.NOT_INITIALISED
+
+        return (
+            InitialisationStatus.FULLY_INITIALISED
+            if (
+                self._task_attributes_register_file.exists()
+                and self._task_hierarchy_graph_file.exists()
+                and self._task_dependency_graph_file.exists()
+                and self._unused_task_file.exists()
+            )
+            else InitialisationStatus.PARTIALLY_INITIALISED
+        )
 
     @override
     def get_unused_task_uid(self) -> tasks.UID:
@@ -248,25 +281,72 @@ class LocalFileDataLayer(architecture.DataLayer):
         return the same value if called multiple times. The returned value will
         only change once a system containing the task uid is saved.
         """
-        return _load_unused_task()
+        unused_task_file = self._data_directory / _UNUSED_TASK_FILENAME
+        formatted_number = unused_task_file.read_text()
+        number = int(formatted_number)
+        return tasks.UID(number)
+
+    def _load_task_system(self) -> tasks.System:
+        attributes_register = self._load_task_attributes_register()
+        hierarchy_graph = self._load_task_hierarchy_graph()
+        dependency_graph = self._load_task_dependency_graph()
+        return tasks.System(
+            attributes_register=attributes_register,
+            hierarchy_graph=hierarchy_graph,
+            dependency_graph=dependency_graph,
+        )
+
+    def _load_task_attributes_register(self) -> tasks.AttributesRegister:
+        with self._task_attributes_register_file.open("r") as fp:
+            return json.load(fp=fp, object_hook=_decode_task_attributes_register)
+
+    def _load_task_hierarchy_graph(self) -> tasks.HierarchyGraph:
+        """Load the task hierarchy graph."""
+        with self._task_hierarchy_graph_file.open("r") as fp:
+            hierarchy_relationships = json.load(
+                fp=fp, object_hook=_decode_task_relationships
+            )
+        return tasks.HierarchyGraph(
+            reduced_dag=graphs.ReducedDAG(
+                bidict=graphs.BiDirectionalSetDict(forward=hierarchy_relationships)
+            )
+        )
+
+    def _load_task_dependency_graph(self) -> tasks.DependencyGraph:
+        """Load the task dependency graph."""
+        with self._task_dependency_graph_file.open("r") as fp:
+            dependency_relationships = json.load(
+                fp=fp, object_hook=_decode_task_relationships
+            )
+        return tasks.DependencyGraph(
+            dag=graphs.DirectedAcyclicGraph(
+                bidict=graphs.BiDirectionalSetDict(forward=dependency_relationships)
+            )
+        )
 
     @override
     def load_system(self) -> domain.System:
-        return _load_system()
+        task_system = self._load_task_system()
+        return domain.System(task_system=task_system)
+
+    def _initialise(self) -> None:
+        self._data_directory.mkdir(parents=True)
+        self._save_data(system=domain.System.empty(), unused_task=_FIRST_TASK)
 
     @override
     def erase(self) -> None:
-        _erase()
+        shutil.rmtree(self._data_directory)
+        self._initialise()
 
     @override
     def save_system(self, system: domain.System) -> None:
-        _save_data(system=system)
+        self._save_data(system=system)
 
     @override
     def save_system_and_indicate_task_used(
         self, system: domain.System, used_task: tasks.UID
     ) -> None:
-        current_unused_task = _load_unused_task()
+        current_unused_task = self.get_unused_task_uid()
 
         if used_task != current_unused_task:
             # TODO: Add better Exception
@@ -276,99 +356,34 @@ class LocalFileDataLayer(architecture.DataLayer):
             current_unused_task=current_unused_task, system=system
         )
 
-        _save_data(system=system, unused_task=new_unused_task)
+        self._save_data(system=system, unused_task=new_unused_task)
 
-
-def _save_data(system: domain.System, unused_task: tasks.UID | None = None) -> None:
-    """Save the system and update the unused task file if necessary."""
-    formatted_task_hierarchy_graph = json.dumps(
-        obj=system.task_system().hierarchy_graph().task_subtasks_pairs(),
-        default=_encode_task_relationships,
-    )
-    formatted_task_dependency_graph = json.dumps(
-        obj=system.task_system().dependency_graph().task_dependents_pairs(),
-        default=_encode_task_relationships,
-    )
-    formatted_task_attributes_register = json.dumps(
-        obj=system.task_system().attributes_register(),
-        default=_encode_task_attributes_register,
-    )
-
-    files_with_text = [
-        (_TASK_HIERARCHY_GRAPH_FILEPATH, formatted_task_hierarchy_graph),
-        (_TASK_DEPENDENCY_GRAPH_FILEPATH, formatted_task_dependency_graph),
-        (_TASK_ATTRIBUTES_REGISTER_FILEPATH, formatted_task_attributes_register),
-    ]
-
-    if unused_task:
-        files_with_text.append((_UNUSED_TASK_FILEPATH, _encode_task_uid(unused_task)))
-
-    _save_file_group_atomically(files_with_text=files_with_text)
-
-
-def _initialise() -> None:
-    """Initialise the local file data-layer."""
-    _DATA_DIRECTORY_PATH.mkdir()
-    _save_data(system=domain.System.empty(), unused_task=_FIRST_TASK)
-
-
-def _erase() -> None:
-    """Erase all data."""
-    shutil.rmtree(_DATA_DIRECTORY_PATH)
-    _initialise()
-
-
-def _load_unused_task() -> tasks.UID:
-    formatted_number = _UNUSED_TASK_FILEPATH.read_text()
-    number = int(formatted_number)
-    return tasks.UID(number)
-
-
-def _load_task_attributes_register() -> tasks.AttributesRegister:
-    """Load the task attributes register."""
-    with _TASK_ATTRIBUTES_REGISTER_FILEPATH.open("r") as fp:
-        return json.load(fp=fp, object_hook=_decode_task_attributes_register)
-
-
-def _load_task_system() -> tasks.System:
-    """Load the task system."""
-    attributes_register = _load_task_attributes_register()
-    hierarchy_graph = _load_task_hierarchy_graph()
-    dependency_graph = _load_task_dependency_graph()
-    return tasks.System(
-        attributes_register=attributes_register,
-        hierarchy_graph=hierarchy_graph,
-        dependency_graph=dependency_graph,
-    )
-
-
-def _load_task_hierarchy_graph() -> tasks.HierarchyGraph:
-    """Load the task hierarchy graph."""
-    with _TASK_HIERARCHY_GRAPH_FILEPATH.open("r") as fp:
-        hierarchy_relationships = json.load(
-            fp=fp, object_hook=_decode_task_relationships
+    def _save_data(
+        self, system: domain.System, unused_task: tasks.UID | None = None
+    ) -> None:
+        """Save the system and update the unused task file if necessary."""
+        formatted_task_hierarchy_graph = json.dumps(
+            obj=system.task_system().hierarchy_graph().task_subtasks_pairs(),
+            default=_encode_task_relationships,
         )
-    return tasks.HierarchyGraph(
-        reduced_dag=graphs.ReducedDAG(
-            bidict=graphs.BiDirectionalSetDict(forward=hierarchy_relationships)
+        formatted_task_dependency_graph = json.dumps(
+            obj=system.task_system().dependency_graph().task_dependents_pairs(),
+            default=_encode_task_relationships,
         )
-    )
-
-
-def _load_task_dependency_graph() -> tasks.DependencyGraph:
-    """Load the task dependency graph."""
-    with _TASK_DEPENDENCY_GRAPH_FILEPATH.open("r") as fp:
-        dependency_relationships = json.load(
-            fp=fp, object_hook=_decode_task_relationships
+        formatted_task_attributes_register = json.dumps(
+            obj=system.task_system().attributes_register(),
+            default=_encode_task_attributes_register,
         )
-    return tasks.DependencyGraph(
-        dag=graphs.DirectedAcyclicGraph(
-            bidict=graphs.BiDirectionalSetDict(forward=dependency_relationships)
-        )
-    )
 
+        files_with_text = [
+            (self._task_hierarchy_graph_file, formatted_task_hierarchy_graph),
+            (self._task_dependency_graph_file, formatted_task_dependency_graph),
+            (self._task_attributes_register_file, formatted_task_attributes_register),
+        ]
 
-def _load_system() -> domain.System:
-    """Load the system."""
-    task_system = _load_task_system()
-    return domain.System(task_system=task_system)
+        if unused_task:
+            files_with_text.append(
+                (self._unused_task_file, _encode_task_uid(unused_task))
+            )
+
+        _save_file_group_atomically(files_with_text=files_with_text)
