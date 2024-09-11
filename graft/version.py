87fb7c3e15db777db90b9a1b51c302f6d08835c2
Matt Woolley <matt.wool@live.com.au>
2024-09-11 20:03:02 +1000

    Update dependencies

--- graft/version.py
+++ graft/version.py
@@ -1,4 +1,6 @@
-# Current version of the app
+"""Current version of the app."""
+# TODO: Find a way to keep this automatically in sync with git tags. It'd be
+# even nicer to have dirty commit hashes visible as well.
 
 from typing import Final
 
